b89dafd86924bf59e22883714d2f6b62f8305290
saleisha57 <saleisha57@yahoo.com>
2017-11-14 13:48:56 -0800

    Add files via upload

---
+++ Enigma/AddTwo.asm
@@ -0,0 +1,21 @@
+; AddTwo.asm - adds two 32-bit integers.
+; Chapter 3 example
+
+;.386
+;.model flat,stdcall
+;.stack 4096
+;ExitProcess proto,dwExitCode:dword
+
+
+INCLUDE Irvine32.inc
+
+.data
+
+.code
+main proc
+	mov	eax,5				
+	add	eax,6				
+
+	invoke ExitProcess,0
+main endp
+end main
